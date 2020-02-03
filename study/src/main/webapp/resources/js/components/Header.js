353268ee8e77b018db220488d8116d4c4403ca59
Sumin <Sumin@DESKTOP-O7UD9KM>
2020-02-03 17:55:38 +0900

    게시판, 댓글, 좋아요, DB설계, view, 게시판 상세, 수정, 삭제 작업 중

--- study/src/main/webapp/resources/js/components/Header.js
+++ study/src/main/webapp/resources/js/components/Header.js
@@ -6,11 +6,41 @@ import header from "../../scss/header.scss";
 const headerStyle = classNames.bind(header);
 
 class Header extends Component {
-	
+
+	constructor(props){
+
+		super(props);
+		this.state = {
+            username: document.getElementById("username").value,
+            gIdx: document.getElementById("gIdx").value,
+            gmIdx: document.getElementById("gmIdx").value,
+		}
+
+	}
+
 	componentDidMount() {
 
+		let username = this.state.username;
+		let htmlTag = document.getElementsByTagName("html")[0];
 		let headerCateTag = document.getElementsByClassName("headerLink");
 		let locPath = location.pathname;
+
+		if(username == null || username == "" || username == "undefined"){
+            
+            const loggedIn = document.getElementById("logged_in");
+			loggedIn.classList.add("active");
+			
+        } else {
+
+            const loggedOut = document.getElementById("logged_out");
+			const profileName = document.getElementById("profile_username");
+			const ddName = document.getElementById("dropdown_username");
+			
+            loggedOut.classList.add("active");
+			profileName.innerHTML = username;
+			ddName.innerHTML = username;
+
+        }
 
 		for(let t=0; t<headerCateTag.length; t++){
 			
@@ -18,12 +48,27 @@ class Header extends Component {
 			headerCatePath == locPath ? headerCateTag[t].classList.add("active") : headerCateTag[t].classList.remove("active");
 
 		}
-		
+
+		const loggedOut = document.getElementById("logged_out");
+		const dropdown = document.getElementById("dropdown_wrap");
+
+		loggedOut.addEventListener("click", (e) => {
+			loggedOut.parentNode.className == "dropdown" ? loggedOut.parentNode.classList.remove("dropdown") : loggedOut.parentNode.classList.add("dropdown")
+			dropdown.className == "dropdown" ? dropdown.classList.remove("dropdown") : dropdown.classList.add("dropdown");
+		});
+
+		const dropdownDetail = document.getElementsByClassName("dropdown_detail");
+		for(let i=0; i<dropdownDetail.length; i++){
+			dropdownDetail[i].addEventListener("click", (e) => {
+				const dataloc = dropdownDetail[i].getAttribute("dataloc");
+				console.log("dataloc = " + dataloc);
+			});
+		}
+
 	}
 
 	headerCateClick = (e) => {
-		//console.log(e.currentTarget.parentNode);
-		
+
 		let clickOrder = e.currentTarget.getAttribute("h_order");
 		let sectionTag = document.getElementsByTagName("section");
 		let sectionHTotal = 0;
@@ -111,20 +156,6 @@ class Header extends Component {
 		
 		return (
 			<div>
-				{/*
-				<Navbar color="dark" dark expand="md">
-					<NavbarBrand href="/study/client/">Client</NavbarBrand>
-					<NavbarBrand href="/study/style">Style</NavbarBrand>
-					<NavbarBrand href="/study/upload">Upload</NavbarBrand>
-					<NavbarBrand href="/study/swiper">Swiper</NavbarBrand>
-					<NavbarBrand href="/study/data">Algorithm</NavbarBrand>
-					<Nav className="ml-auto" navbar>
-						<NavItem>
-							<NavLink href="/study/client/">Home</NavLink>
-						</NavItem>
-					</Nav>
-				</Navbar>
-				*/}
 
 				<header id={headerStyle('header')}>
 					<div className={headerStyle('inner_header')}>
@@ -150,6 +181,21 @@ class Header extends Component {
 							</ul>
 						</div>
 
+						<div id="header_login">
+
+							<div className={headerStyle('header_profile')} id="logged_in">
+								<a href="/study/login">Login</a>
+								<a>Sign up</a>
+							</div>
+
+							<div className={headerStyle('header_profile')} id="logged_out">
+								<img id="profile_default" src="/study/resources/img/port_profile_default.png"></img>
+								<span id="profile_username"></span>
+								<img id="profile_icon" src="/study/resources/img/f107-icon.png"></img>
+							</div>
+
+						</div>
+
 						<div className={headerStyle('m_header_menu')} onClick={this.headerMenu}>
 							<i id="firstIcon"></i>
 							<i id="secondIcon"></i>
@@ -157,6 +203,64 @@ class Header extends Component {
 						</div>
 
 					</div>
+
+					<div id="profile_dropdown">
+						<div id="dropdown_container">
+							<div id="dropdown_wrap">
+
+								<div id="dropdown_profile" className="dropdown_detail" dataloc="timeline">
+									<a>
+										<img id="dropdown_default" src="/study/resources/img/port_profile_default.png"></img>
+										<span id="dropdown_username"></span>
+									</a>
+								</div>
+
+								<hr />
+
+								<ul id="dropdown_info">
+									<li className="dropdown_detail" dataloc="myInfo">
+										<img id="dropdown_default" src="/study/resources/img/port_profile_info.gif" style={{opacity: 1, borderRadius: "50%",}}></img>
+										<span>내 정보</span>
+									</li>
+									<li className="dropdown_detail" dataloc="cart">
+										<img id="dropdown_default" src="/study/resources/img/port_cart.png"></img>
+										<span>장바구니</span>
+									</li>
+
+									<hr />
+
+									<li className="dropdown_detail" dataloc="alarm">
+										<img id="dropdown_default" src="/study/resources/img/port_alarm.png"></img>
+										<span>알림</span>
+									</li>
+									<li className="dropdown_detail" dataloc="message">
+										<img id="dropdown_default" src="/study/resources/img/port_message.png"></img>
+										<span>쪽지</span>
+									</li>
+
+									<hr />
+
+									<li className="dropdown_detail" dataloc="service">
+										<img id="dropdown_default" src="/study/resources/img/port_question.png"></img>
+										<span>고객센터</span>
+									</li>
+									<li className="dropdown_detail" dataloc="report">
+										<img id="dropdown_default" src="/study/resources/img/port_alarm.png"></img>
+										<span>신고센터</span>
+									</li>
+
+									<hr />
+
+									<li className="dropdown_detail" dataloc="logout">
+										<img id="dropdown_default" src="/study/resources/img/port_logout.png"></img>
+										<span>로그아웃</span>
+									</li>
+
+								</ul>
+							</div>
+						</div>
+					</div>
+
 				</header>
 
 			</div>
